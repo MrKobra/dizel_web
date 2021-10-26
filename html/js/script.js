5c8c4fea0fe2fb1c9733efcaff6a5f008f70bc70
Mr-Kobra <red.kras.2013@gmail.com>
2021-10-26 23:43:27 +0300

    Add nav scripts

--- html/js/script.js
+++ html/js/script.js
@@ -14,4 +14,63 @@ $(document).ready(function(){
             $(this).parent().find('label').removeClass('active');
         }
     })
+    // Кнопка вверх
+    $('.up-arrow').on('click', function() {
+        $('html, body').animate({
+            scrollTop: $('.top-header').offset().top
+        }, 500)
+    })
+    // Изменение активной ссылки при прокрутке
+    function setHeight(){
+        var menuOffset = $('.top-nav').outerHeight();
+        $('.top-nav a').each(function(){
+            var href = $(this).attr('href');
+            var start = $(href).offset().top - menuOffset;
+            $(this).data('start', start);
+            $(this).data('end', start + $(href).outerHeight());
+        })
+    }
+    setHeight();
+    function activeItem(){
+        var windowScroll = $(window).scrollTop();
+        $('.top-nav li').removeClass('active')
+        $('.top-nav a').each(function(){
+            if(windowScroll > $(this).data('start') && windowScroll < $(this).data('end')) {
+                $(this).parent().addClass('active');
+            }
+        })
+    }
+    activeItem();
+    // Фиксация меню при сколле
+    function fixMenu() {
+        var windowScroll = $(window).scrollTop();
+        if(windowScroll > $('.top-header').outerHeight()) {
+            $('.top-nav').addClass('fixed');
+            $('body').css('padding-top', $('.top-nav').outerHeight());
+        } else {
+            $('.top-nav').removeClass('fixed');
+            $('body').css('padding-top', 0)
+        }
+    }
+    $(window).scroll(function (){
+        activeItem();
+        fixMenu();
+    })
+    // Прокрутка до пункта
+    $('.top-nav a').on('click', function(e){
+        e.preventDefault();
+        var menuOffset = $('.top-nav').outerHeight() + 2;
+        if($(window).width() > 992) {
+            var href = $(this).attr('href');
+            $('html, body').animate({
+                scrollTop: $(href).offset().top,
+            }, 500)
+        }
+    })
+    // Попап окно
+    $('.request-btn').each(function (){
+        $(this).magnificPopup({
+            type:'inline',
+        });
+    })
 })
